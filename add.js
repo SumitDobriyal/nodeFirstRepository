0a63bf1d881ec845d31c347d76b372611fad78fa
Sumit <dobriyal.sumit@gmail.com>
2019-06-18 16:09:13 +0530

    initial commit

---
+++ add.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+console.log(fs);
+
+
+
+const add = (num1, num2) => {
+	return num1 + num2;
+}
+
+module.exports = add;
---
+++ app.js
@@ -0,0 +1,14 @@
+// in es6  = import add from './actual path';
+const add = require('./add');
+//const fs = require('fs');
+
+console.log(add(6,12));
+//console.log(fs);
+
+// importing a module process below
+// resolve
+// load
+// wrap
+// cache
+
+
